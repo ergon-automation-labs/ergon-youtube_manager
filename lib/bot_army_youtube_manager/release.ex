d446497e6afea4a00f4c8ef25b459cc3006e83fa
Abby Malson <august.malson@gmail.com>
2026-05-20 10:36:33 -0600

    feat: Add setup() and improve migrate() in Release module
    Add database creation via Ecto before running migrations. Public
    migrate() function can be called directly from Salt deployments.

--- lib/bot_army_youtube_manager/release.ex
+++ lib/bot_army_youtube_manager/release.ex
@@ -5,21 +5,31 @@ defmodule BotArmyYoutubeManager.Release do
 
   require Logger
 
-  def prepare do
+  def setup do
     load_app()
+    create_database()
     migrate()
+  end
+
+  def migrate do
+    load_app()
+    {:ok, _} = Application.ensure_all_started(:ecto_sql)
+    repos = Application.fetch_env!(:bot_army_youtube_manager, :ecto_repos)
+    Enum.each(repos, &migrate_repo/1)
   end
 
   defp load_app do
     Application.ensure_all_started(:bot_army_youtube_manager)
   end
 
-  defp migrate do
+  defp create_database do
     {:ok, _} = Application.ensure_all_started(:ecto_sql)
 
-    repos = Application.fetch_env!(:bot_army_youtube_manager, :ecto_repos)
-
-    Enum.each(repos, &migrate_repo/1)
+    case Ecto.Adapters.Postgres.storage_up(BotArmyYoutubeManager.Repo.config()) do
+      :ok -> Logger.info("Database created successfully")
+      {:error, :already_up} -> Logger.info("Database already exists")
+      {:error, reason} -> Logger.warning("Database creation failed: #{inspect(reason)}")
+    end
   end
 
   defp migrate_repo(repo) do
