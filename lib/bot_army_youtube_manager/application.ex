798db269af3bc515229db31e4afe58b7e5866fd9
Abby Malson <august.malson@gmail.com>
2026-08-25 11:26:28 +0300

    refactor: rename LEADER_NODE_NAME to NODE_NAME

--- lib/bot_army_youtube_manager/application.ex
+++ lib/bot_army_youtube_manager/application.ex
@@ -58,7 +58,7 @@ defmodule BotArmyYoutubeManager.Application do
         # while the consumer is still connecting (not yet subscribed either way).
         {BotArmyLibraryRuntime.LeaderElection,
          service: "youtube_manager",
-         node_name: System.get_env("LEADER_NODE_NAME", "unknown"),
+         node_name: System.get_env("NODE_NAME", "unknown"),
          default_role:
            BotArmyLibraryRuntime.LeaderElection.role_from_env("YOUTUBE_MANAGER_NODE_ROLE"),
          on_role_change: {BotArmyYoutubeManager.NATS.Consumer, :leader_role_changed, []}},
